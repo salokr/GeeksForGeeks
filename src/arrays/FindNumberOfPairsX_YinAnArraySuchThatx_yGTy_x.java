59de0bac7d3fd3ffc30d5da11ad9b63a86e9ebc0
ashutosh <ashutosh@192.168.0.104>
2016-06-20 02:41:55 +0530

    GFS till 12/06/16

---
+++ src/arrays/FindNumberOfPairsX_YinAnArraySuchThatx_yGTy_x.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author ashutosh
+ */
+/*
+Find number of pairs (x, y) in an array such that x^y > y^x
+Given two arrays X[] and Y[] of positive integers, find number of pairs such that x^y > y^x where x is an element from X[] and y is an element from Y[].
+
+Examples:
+
+  Input: X[] = {2, 1, 6}, Y = {1, 5}
+  Output: 3 
+  // There are total 3 pairs where pow(x, y) is greater than pow(y, x)
+  // Pairs are (2, 1), (2, 5) and (6, 1)
+
+
+  Input: X[] = {10, 19, 18}, Y[] = {11, 15, 9};
+  Output: 2
+  // There are total 2 pairs where pow(x, y) is greater than pow(y, x)
+  // Pairs are (10, 11) and (10, 15)
+*/
+public class FindNumberOfPairsX_YinAnArraySuchThatx_yGTy_x {
+    
+}
