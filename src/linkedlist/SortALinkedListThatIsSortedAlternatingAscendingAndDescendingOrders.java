ed58a3145e0d9ad8b7c8194f61c44806aef8dbfa
ashutosh <ashutosh@192.168.1.118>
2016-07-14 02:35:21 +0530

    GFS till 12/06/16

---
+++ src/linkedlist/SortALinkedListThatIsSortedAlternatingAscendingAndDescendingOrders.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author ashutosh
+Sort a linked list that is sorted alternating ascending and descending orders?
+Given a Linked List. The Linked List is in alternating ascending and descending orders. Sort the list efficiently.
+
+Example:
+
+Input List:   10->40->53->30->67->12->89->NULL
+Output List:  10->12->30->43->53->67->89->NULL
+Source: http://qa.geeksforgeeks.org/616/linked-that-sorted-alternating-ascending-descending-orders
+
+We strongly recommend you to minimize your browser and try this yourself first.
+
+A Simple Solution is to use Merge Sort for linked List. This solution takes O(n Log n) time.
+*/
+public class SortALinkedListThatIsSortedAlternatingAscendingAndDescendingOrders
+{
+    
+}
